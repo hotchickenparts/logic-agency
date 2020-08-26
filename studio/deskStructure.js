694c5c29c358402c5708c9e363abd3bec86db9ef
Fitzpatrick Nobles <fitz@hotchickenparts.com>
2020-08-25 18:14:34 -0600

    modified document project, added icons

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,36 +1,36 @@
-import S from '@sanity/desk-tool/structure-builder'
-import MdSettings from 'react-icons/lib/md/settings'
+import S from "@sanity/desk-tool/structure-builder";
+import { FcSettings } from "react-icons/fc";
 
 const hiddenDocTypes = listItem =>
-  !['category', 'person', 'sampleProject', 'siteSettings'].includes(listItem.getId())
+  !["category", "person", "project", "siteSettings"].includes(listItem.getId());
 
 export default () =>
   S.list()
-    .title('Content')
+    .title("Content")
     .items([
       S.listItem()
-        .title('Settings')
+        .title("Settings")
         .child(
           S.editor()
-            .id('siteSettings')
-            .schemaType('siteSettings')
-            .documentId('siteSettings')
+            .id("siteSettings")
+            .schemaType("siteSettings")
+            .documentId("siteSettings")
         )
-        .icon(MdSettings),
+        .icon(FcSettings),
       S.listItem()
-        .title('Sample projects')
-        .schemaType('sampleProject')
-        .child(S.documentTypeList('sampleProject').title('Sample projects')),
+        .title("Projects")
+        .schemaType("project")
+        .child(S.documentTypeList("project").title("Projects")),
       S.listItem()
-        .title('People')
-        .schemaType('person')
-        .child(S.documentTypeList('person').title('People')),
+        .title("People")
+        .schemaType("person")
+        .child(S.documentTypeList("person").title("People")),
       S.listItem()
-        .title('Categories')
-        .schemaType('category')
-        .child(S.documentTypeList('category').title('Categories')),
+        .title("Categories")
+        .schemaType("category")
+        .child(S.documentTypeList("category").title("Categories")),
       // This returns an array of all the document types
       // defined in schema.js. We filter out those that we have
       // defined the structure above
       ...S.documentTypeListItems().filter(hiddenDocTypes)
-    ])
+    ]);
--- studio/schemas/documents/category.js
+++ studio/schemas/documents/category.js
@@ -1,17 +1,20 @@
+import { FcTreeStructure } from "react-icons/fc";
+
 export default {
-  name: 'category',
-  type: 'document',
-  title: 'Category',
+  name: "category",
+  type: "document",
+  title: "Category",
+  icon: FcTreeStructure,
   fields: [
     {
-      name: 'title',
-      type: 'string',
-      title: 'Title'
+      name: "title",
+      type: "string",
+      title: "Title"
     },
     {
-      name: 'description',
-      type: 'text',
-      title: 'Description'
+      name: "description",
+      type: "text",
+      title: "Description"
     }
   ]
-}
+};
